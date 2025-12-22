30cf6ccbaed357a51c6090ccce13b8f13f9ef1a5
Emeraldtheporcupine <125104247+Emeraldtheporcupine@users.noreply.github.com>
2025-12-21 22:22:51 -0600

    Made Rings

--- main.ts
+++ main.ts
@@ -1,12 +1,103 @@
 namespace SpriteKind {
     export const Collision = SpriteKind.create()
     export const Texture = SpriteKind.create()
+    export const RingCollect = SpriteKind.create()
+}
+function PlaceTiles () {
+    for (let slopeRight of tiles.getTilesByType(assets.tile`myTile1`)) {
+        rSlope = sprites.create(assets.image`GroundSlopeRight`, SpriteKind.Collision)
+        tiles.placeOnTile(rSlope, slopeRight)
+        tiles.setTileAt(slopeRight, assets.tile`transparency16`)
+    }
+    for (let slopeRight of tiles.getTilesByType(assets.tile`myTile14`)) {
+        rSlope = sprites.create(assets.image`GroundSlopeRight0`, SpriteKind.Collision)
+        tiles.placeOnTile(rSlope, slopeRight)
+        tiles.setTileAt(slopeRight, assets.tile`myTile10`)
+    }
+    for (let slopeLeft of tiles.getTilesByType(assets.tile`myTile2`)) {
+        LSlope = sprites.create(assets.image`GroundSlopeLeft`, SpriteKind.Collision)
+        tiles.placeOnTile(LSlope, slopeLeft)
+        tiles.setTileAt(slopeLeft, assets.tile`transparency16`)
+    }
+    for (let BelowGround of tiles.getTilesByType(assets.tile`myTile3`)) {
+        bGround = sprites.create(assets.image`GroundBelow`, SpriteKind.Collision)
+        tiles.placeOnTile(bGround, BelowGround)
+        tiles.setTileAt(BelowGround, assets.tile`transparency16`)
+    }
+    for (let BelowGround of tiles.getTilesByType(assets.tile`myTile16`)) {
+        bGround = sprites.create(assets.image`ABOVE`, SpriteKind.Collision)
+        tiles.placeOnTile(bGround, BelowGround)
+        tiles.setTileAt(BelowGround, assets.tile`transparency16`)
+    }
+    for (let RingOut of tiles.getTilesByType(assets.tile`myTile18`)) {
+        Ring = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.RingCollect)
+        animation.runImageAnimation(
+        Ring,
+        assets.animation`Ring`,
+        150,
+        true
+        )
+        Ring.x += 2
+        Ring.y += 2
+        tiles.placeOnTile(Ring, RingOut)
+        tiles.setTileAt(RingOut, assets.tile`transparency16`)
+    }
+    for (let AboveGround of tiles.getTilesByType(assets.tile`myTile`)) {
+        aGround = sprites.create(assets.image`Ground`, SpriteKind.Collision)
+        tiles.placeOnTile(aGround, AboveGround)
+        tiles.setTileAt(AboveGround, assets.tile`transparency16`)
+    }
+    for (let AboveGround2 of tiles.getTilesByType(assets.tile`myTile8`)) {
+        aGround = sprites.create(assets.image`GroundBelow0`, SpriteKind.Collision)
+        tiles.placeOnTile(aGround, AboveGround2)
+        tiles.setTileAt(AboveGround2, assets.tile`transparency16`)
+    }
+    for (let AboveGround3 of tiles.getTilesByType(assets.tile`myTile9`)) {
+        aGround = sprites.create(assets.image`GroundBelow1`, SpriteKind.Collision)
+        tiles.placeOnTile(aGround, AboveGround3)
+        tiles.setTileAt(AboveGround3, assets.tile`transparency16`)
+    }
+    for (let Trees of tiles.getTilesByType(assets.tile`myTile5`)) {
+        Texture2 = sprites.create(assets.image`Tree`, SpriteKind.Texture)
+        tiles.placeOnTile(Texture2, Trees)
+        Texture2.y += -12
+        Texture2.z = -20
+        tiles.setTileAt(Trees, assets.tile`myTile4`)
+    }
+    for (let soil of tiles.getTilesByType(assets.tile`myTile4`)) {
+        Texture2 = sprites.create(assets.image`Grass`, SpriteKind.Texture)
+        tiles.placeOnTile(Texture2, soil)
+        Texture2.z = 20
+        tiles.setTileAt(soil, assets.tile`transparency16`)
+    }
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Sonic.vy == 0) {
         Sonic.vy = -125
         music.play(music.createSoundEffect(WaveShape.Square, 1, 2394, 255, 0, 200, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
     }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.RingCollect, function (sprite, otherSprite) {
+    music.setVolume(50)
+    music.play(music.createSong(assets.song`Ring`), music.PlaybackMode.InBackground)
+    sprites.destroy(otherSprite)
 })
 function SetAnim () {
     characterAnimations.loopFrames(
@@ -59,6 +150,7 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Collision, function (sprite, oth
 let Direction = 0
 let Texture2: Sprite = null
 let aGround: Sprite = null
+let Ring: Sprite = null
 let bGround: Sprite = null
 let LSlope: Sprite = null
 let rSlope: Sprite = null
@@ -70,51 +162,9 @@ scene.cameraFollowSprite(Sonic)
 characterAnimations.setCharacterState(Sonic, characterAnimations.rule(Predicate.NotMoving, Predicate.FacingRight))
 scene.setBackgroundImage(assets.image`Night`)
 tiles.setCurrentTilemap(tileUtil.createSmallMap(tilemap`level2`))
-music.play(music.createSong(assets.song`Zone`), music.PlaybackMode.LoopingInBackground)
+music.play(music.createSong(assets.song`Sleepy Hill Zone`), music.PlaybackMode.LoopingInBackground)
 tiles.placeOnTile(Sonic, tiles.getTileLocation(1, 13))
-for (let slopeRight of tiles.getTilesByType(assets.tile`myTile1`)) {
-    rSlope = sprites.create(assets.image`GroundSlopeRight`, SpriteKind.Collision)
-    tiles.placeOnTile(rSlope, slopeRight)
-    tiles.setTileAt(slopeRight, assets.tile`transparency16`)
-}
-for (let slopeLeft of tiles.getTilesByType(assets.tile`myTile2`)) {
-    LSlope = sprites.create(assets.image`GroundSlopeLeft`, SpriteKind.Collision)
-    tiles.placeOnTile(LSlope, slopeLeft)
-    tiles.setTileAt(slopeLeft, assets.tile`transparency16`)
-}
-for (let BelowGround of tiles.getTilesByType(assets.tile`myTile3`)) {
-    bGround = sprites.create(assets.image`GroundBelow`, SpriteKind.Collision)
-    tiles.placeOnTile(bGround, BelowGround)
-    tiles.setTileAt(BelowGround, assets.tile`transparency16`)
-}
-for (let AboveGround of tiles.getTilesByType(assets.tile`myTile`)) {
-    aGround = sprites.create(assets.image`Ground`, SpriteKind.Collision)
-    tiles.placeOnTile(aGround, AboveGround)
-    tiles.setTileAt(AboveGround, assets.tile`transparency16`)
-}
-for (let AboveGround2 of tiles.getTilesByType(assets.tile`myTile8`)) {
-    aGround = sprites.create(assets.image`GroundBelow0`, SpriteKind.Collision)
-    tiles.placeOnTile(aGround, AboveGround2)
-    tiles.setTileAt(AboveGround2, assets.tile`transparency16`)
-}
-for (let AboveGround3 of tiles.getTilesByType(assets.tile`myTile9`)) {
-    aGround = sprites.create(assets.image`GroundBelow1`, SpriteKind.Collision)
-    tiles.placeOnTile(aGround, AboveGround3)
-    tiles.setTileAt(AboveGround3, assets.tile`transparency16`)
-}
-for (let Trees of tiles.getTilesByType(assets.tile`myTile5`)) {
-    Texture2 = sprites.create(assets.image`Tree`, SpriteKind.Texture)
-    tiles.placeOnTile(Texture2, Trees)
-    Texture2.y += -12
-    Texture2.z = -20
-    tiles.setTileAt(Trees, assets.tile`myTile4`)
-}
-for (let soil of tiles.getTilesByType(assets.tile`myTile4`)) {
-    Texture2 = sprites.create(assets.image`Grass`, SpriteKind.Texture)
-    tiles.placeOnTile(Texture2, soil)
-    Texture2.z = 20
-    tiles.setTileAt(soil, assets.tile`transparency16`)
-}
+PlaceTiles()
 game.onUpdate(function () {
     if (controller.right.isPressed()) {
         Direction = 1
